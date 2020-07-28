e88e1582dbfbaa2d46cb051fa3c1cb2435725a46
tcortes55 <35512873+tcortes55@users.noreply.github.com>
2020-07-27 22:43:12 -0300

    Initial commit

---
+++ src/components/Menu/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Logo from '../../assets/img/primeflix.png';
+import './Menu.css'
+import ButtonLink from './components/ButtonLink';
+
+
+function Menu() {
+    return (
+        <nav className="Menu">
+            <a href="/">
+                <img className="Logo" src={Logo} alt="PrimeFlix logo" />
+            </a>
+
+            <ButtonLink className="ButtonLink" href="/">
+                Novo vídeo
+            </ButtonLink>
+        </nav>
+    );
+}
+
+export default Menu;
