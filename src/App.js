58826147554eb9d81334b43008d2a7c4a730a320
tcortes55 <35512873+tcortes55@users.noreply.github.com>
2020-07-28 21:18:08 -0300

    New components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import Menu from './components/Menu';
+import site_data from './data/site_data.json';
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
       <Menu />
-      <h1>Teste</h1>
-      Testando React.
+      <BannerMain
+        videoTitle="Teste"
+        videoDescription="Teste descrição"
+        url="https://www.youtube.com/watch?v=c8mVlakBESE"
+      />
+      
+      <Footer />
     </div>
   );
 }
