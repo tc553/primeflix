82438edbe1af1bda9406f6fa26f7da31a4f9fbfd
tcortes55 <35512873+tcortes55@users.noreply.github.com>
2020-08-04 00:43:07 -0300

    New category form

--- src/pages/register/category/index.js
+++ src/pages/register/category/index.js
@@ -1,12 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DefaultPage from '../../../components/DefaultPage'
 import { Link } from 'react-router-dom';
 
 function RegisterCategory() {
+    const initialValues = {
+        name: '',
+        description: '',
+        color: ''
+    }
+
+    let [categories, setCategories] = useState([]);
+    let [values, setValues] = useState(initialValues);
+
+    function handleInput(e) {
+        const { getAttribute, value } = e.target;
+        setValue(getAttribute('name'), value);
+    }
+
+    function setValue(key, value) {
+        setValues({
+            ...values,
+            [key]: value
+        });
+    }
+
     return (
         <DefaultPage>
-            <h1>Register category</h1>
+            <h1>Cadastrar categoria: { values.name }</h1>
             <Link to="/">Home</Link>
+
+            <form onSubmit={function handleSubmit(e) {
+                e.preventDefault();
+                setCategories([
+                    ...categories,
+                    values
+                ]);
+
+                setValues(initialValues);
+            }}>
+                <div>
+                    <label>
+                        Nome da categoria:
+                        <input
+                            type="text"
+                            name="name"
+                            value={values.name}
+                            onChange={handleInput}
+                        />
+                    </label>
+                </div>
+
+                <div>    
+                    <label>
+                        Descrição:
+                        <textarea
+                            type="text"
+                            name="description"
+                            value={values.description}
+                            onChange={handleInput}
+                        />
+                    </label>
+                </div>
+                    
+                <div>
+                    <label>
+                        Cor:
+                        <input
+                            type="color"
+                            name="color"
+                            value={values.color}
+                            onChange={handleInput}
+                        />
+                    </label>
+                </div>
+
+                <button>Cadastrar</button>
+            </form>
+
+            <ul>
+                { categories.map((category, index) => {
+                    return <li key={index}>{ category.name }</li>
+                }) }
+            </ul>
+
         </DefaultPage>
     )
 }
--- src/pages/register/video/index.js
+++ src/pages/register/video/index.js
@@ -5,10 +5,10 @@ import { Link } from 'react-router-dom';
 function RegisterVideo() {
     return (
         <DefaultPage>
-            <h1>Register video</h1>
+            <h1>Cadastrar vídeo</h1>
             <Link to="/">Home</Link>
             <br />
-            <Link to="/register/category">New category</Link>
+            <Link to="/register/category">Nova categoria</Link>
         </DefaultPage>
     )
 }
